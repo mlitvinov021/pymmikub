6f3deafa56a1f4d59f85efa61826029c1e508f4e
mlitvinov021 <144617915+mlitvinov021@users.noreply.github.com>
2024-09-29 15:56:34 +0200

    Basic project structure based on tutorial project

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+import os
+import tempfile
+
+import pytest
+from pymmikub import create_app
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path,
+    })
+
+    with app.app_context():
+        # Initialize database
+        pass
+
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
